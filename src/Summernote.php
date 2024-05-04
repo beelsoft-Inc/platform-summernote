553d1a934b81d4368c8cccfb3d9e57fedd48841e
Mr Kun <tungnt@ommani.vn>
2024-05-04 16:54:03 +0700

    git init

---
+++ src/Summernote.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tungnt\Summernote;
+
+use Tungnt\Admin\Extension;
+
+class Summernote extends Extension
+{
+    public $name = 'summernote';
+
+    public $views = __DIR__.'/../resources/views';
+
+    public $assets = __DIR__.'/../resources/assets';
+}
